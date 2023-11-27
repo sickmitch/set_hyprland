aa73979726b181551d143f50dedfdd97351fde84
sickmitch <marinic.michele@proton.me>
2023-11-27 14:34:28 +0100

    debian test 2

--- set_hypr_deb.sh
+++ set_hypr_deb.sh
@@ -6,7 +6,7 @@ echo "Starting the installation process..."
 
 # Grabbing dependencies for the build process
 sudo apt update && sudo apt ugrade -y
-sudo apt install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr git hwdata-dev libpango1.0-dev libgbm-dev libsystemd-dev
+sudo apt install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr git hwdata-dev libpango1.0-dev libgbm-dev libsystemd-dev xwayland
 
 # Prep stage 
 mkdir hypr && cd hypr
@@ -16,7 +16,7 @@ tar -xvf source-v0.24.1.tar.gz
 wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.31/downloads/wayland-protocols-1.31.tar.xz
 tar -xvJf wayland-protocols-1.31.tar.xz
 wget https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.22.0/downloads/wayland-1.22.0.tar.xz
-tar -xzvJf wayland-1.22.0.tar.xz
+tar -xzJf wayland-1.22.0.tar.xz
 wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.1.1/downloads/libdisplay-info-0.1.1.tar.xz
 tar -xvJf libdisplay-info-0.1.1.tar.xz
 git clone https://gitlab.freedesktop.org/emersion/libliftoff.git
@@ -55,9 +55,10 @@ meson setup build/
 ninja -C build/
 cd build/
 sudo ninja install 
+cd ../..
 
 # Hyprland
-chmod a+rw hyprland-source
+sudo chmod a+rw hyprland-source
 cd hyprland-source/
 sed -i 's/\/usr\/local/\/usr/g' config.mk 
 sudo make install
