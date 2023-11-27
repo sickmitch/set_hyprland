5e005c8871aac2251cf7b9dff7386b1d2b2cedee
sickmitch <marinic.michele@proton.me>
2023-11-27 15:24:59 +0100

    debian test 3

--- set_hypr_deb.sh
+++ set_hypr_deb.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-echo "You will need root access later..."
-wait 5
-echo "Starting the installation process..."
-
 # Grabbing dependencies for the build process
-sudo apt update && sudo apt ugrade -y
-sudo apt install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr git hwdata-dev libpango1.0-dev libgbm-dev libsystemd-dev xwayland
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr git hwdata libpango1.0-dev libgbm-dev libsystemd-dev xwayland
 
 # Prep stage 
 mkdir hypr && cd hypr
@@ -16,7 +12,7 @@ tar -xvf source-v0.24.1.tar.gz
 wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.31/downloads/wayland-protocols-1.31.tar.xz
 tar -xvJf wayland-protocols-1.31.tar.xz
 wget https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.22.0/downloads/wayland-1.22.0.tar.xz
-tar -xzJf wayland-1.22.0.tar.xz
+tar -xvJf wayland-1.22.0.tar.xz
 wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.1.1/downloads/libdisplay-info-0.1.1.tar.xz
 tar -xvJf libdisplay-info-0.1.1.tar.xz
 git clone https://gitlab.freedesktop.org/emersion/libliftoff.git
